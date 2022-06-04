eb07d021e578e6d8027c25290ae826f236da2c1e
FaithKwamboka <okongofaith3@gmail.com>
2022-06-04 23:40:44 +0300

    Feat:Define register view

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Follow, Like, Post, Profile, Comment
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from  insta_clone import settings
+# from .forms import UpdateUserForm, UpdateProfileForm, AddPostForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+# from .tokens import account_activation_token
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from .models import Follow, Like, Post, Profile, Comment
+from django.core.mail import EmailMessage
+import threading
+
+class EmailThread(threading.Thread):
+    
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+def send_activation_email(user, request):
+    current_site = get_current_site(request)
+    email_subject = 'Activate Your Kwash Gram Account'
+    email_body = render_to_string('Account Activation Email.html', {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        # 'token': account_activation_token.make_token(user)
+    })
+
+    email = EmailMessage(subject=email_subject, body=email_body,
+    from_email=settings.EMAIL_FROM_USER, to=[user.email])
+
+    if not settings.TESTING:
+        EmailThread(email).start()
 
 # Create your views here.
+def Register(request):
+    if request.method == 'POST':
+        context = {'has_error': False}
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, '⚠️ Passwords Do Not Match! Try Again')
+            return redirect('Register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '⚠️ Username Already Exists! Choose Another One')
+            return redirect('Register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, '⚠️ Email Address Already Exists! Choose Another One')
+            return redirect('Register')
+
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email)
+        user.set_password(password1)
+        user.is_active = False
+        user.save()
+
+        if not context['has_error']:
+            send_activation_email(user, request)
+            messages.success(request, '✅ Registration Successful! An Activation Link Has Been Sent To Your Email')
+            return redirect('Register')
+
+    return render(request, 'Register.html')
