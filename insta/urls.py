257649c3b2c7a9a710731ea7f7da0caffd4bfc82
FaithKwamboka <okongofaith3@gmail.com>
2022-06-05 11:03:58 +0300

    Add urls

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('', views.Home, name="Home"),
-    path('login', views.Login, name="Login"),
-    path('register', views.Register, name="Register"),
+    path('', views.home,name='home'),
+    path('signup/$', views.signup, name='signup'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
