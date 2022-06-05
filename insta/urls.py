22ee255624252072db3a7f3a95845505c62a3237
FaithKwamboka <okongofaith3@gmail.com>
2022-06-05 14:37:28 +0300

    Updating urls

--- insta/urls.py
+++ insta/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
+from django.urls import re_path
 from insta import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('signup/$', views.signup, name='signup'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    re_path(r'^$', views.home,name='home'),
+    re_path(r'^signup/$', views.signup, name='signup'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    re_path(r'^accounts/profile/', views.add_profile, name='add_profile'),
+    re_path(r'^profile/(\d+)', views.profile, name='profile'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^image/(\d+)',views.get_image_by_id,name ='image'),
+    re_path(r'^new/profile$', views.add_profile, name='add_profile'),
+    re_path(r'^upload/', views.update_image, name='upload'),
+    re_path(r'^comment/(?P<pk>\d+)',views.add_comment,name='comment'),
+    re_path(r'^like/(?P<operation>.+)/(?P<pk>\d+)',views.like, name='like'),
+    re_path(r'^all/(?P<pk>\d+)', views.all, name='all'),
 ]
