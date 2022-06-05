c40b693c6e7712af46cce0b3b5ed66a37f0b08dd
FaithKwamboka <okongofaith3@gmail.com>
2022-06-05 11:33:25 +0300

    Define account  activation view

--- insta/views.py
+++ insta/views.py
@@ -37,3 +37,17 @@ def signup(request):
     else:
         form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        current_user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        current_user = None
+    if current_user is not None and account_activation_token.check_token(current_user, token):
+        current_user.is_active = True
+        current_user.save()
+        login(request, current_user)
+        return HttpResponse('Thank you for your email confirmation. <a href="https://kwashgram.herokuapp.com"> Login </a> Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
