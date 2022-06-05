41228713318abd43f009cda4d9c95676e5787bfe
FaithKwamboka <okongofaith3@gmail.com>
2022-06-05 11:10:41 +0300

    Update project urls

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -20,5 +20,8 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('insta.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path(include('insta.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/$', views.logout, {"next_page": '/accounts/login/'}),
+    path('tinymce/', include('tinymce.urls')),
+]
