7b12062c81b18f84f3ae3af6fd458278de73f818
FaithKwamboka <okongofaith3@gmail.com>
2022-06-05 10:56:51 +0300

    Updating forms

--- insta/forms.py
+++ insta/forms.py
@@ -1,30 +1,24 @@
 from django import forms
-from django.contrib.auth.models import User
-from .models import Comment, Post, Profile
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
-class UpdateUserForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    username = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email']
-
-class UpdateProfileForm(forms.ModelForm):
-    profile_image = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    bio = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-
+class NewProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_image', 'bio']
+        exclude = ['user']
 
-class AddPostForm(forms.ModelForm):
-    image = forms.ImageField(required=True, widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    title = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}))
-    caption = forms.CharField(max_length=2200, required=True, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Caption'}))
-
+class UploadForm(forms.ModelForm):
     class Meta:
-        model = Post
-        fields = ['image', 'title', 'caption']
+        model = Image
+        exclude =['posted_by','profile','likes']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude =['poster','image']
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
